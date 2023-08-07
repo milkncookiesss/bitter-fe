c9482b09005d0ca30dc373827a1be5ecad1397c6
Peter Park <p.parkcontact@gmail.com>
2023-08-06 20:25:52 -0700

    "update post create component and remove manifest items"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,15 @@
 import '../componentCss/App.css';
+import PostCreateComponent from './PostCreate.jsx';
+import { useState } from 'react';
 
 function App() {
+  //gobal state(?)
+  //pass state down to components
+  const [posts, setPosts] = useState([{username: "lalala", post: "aaaaaaaaaaaaaa"},{username: "nanana", post:"bbbbbbbbbb"}]);
+
   return (
     <div className="App">
+      <PostCreateComponent posts={posts}/>
       <p>hi fam</p>
     </div>
   );
---
+++ src/components/PostCreate.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+function PostCreateComponent({ posts }) {
+  console.log("wtf is this shit =================> ", posts);
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default PostCreateComponent;
